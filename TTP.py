2640258026efa3479149c4f8b3ef983de9557858
OdyMeister <onixbl@gmail.com>
2024-04-17 11:59:26 +0200

    Initial commit

---
+++ TTP.py
@@ -0,0 +1,140 @@
+import sys
+import numpy as np
+import copy
+
+COUNT = 0
+
+# def generate_schedules(n, matchups, schedule, team=0):
+#     # If team is equal to n, print the schedule and return
+#     if team == n:
+#         # for i in range(len(schedule)):
+#         #     print(i, schedule[i])
+#         # print()
+#         global COUNT
+#         COUNT += 1
+#         return
+    
+#     # If there are no matchups for the team, increment team and call generate_schedules
+#     if len(matchups[team]) == 0:
+#         team += 1
+#         generate_schedules(n, matchups, schedule, team)
+#         return
+    
+#     for m in matchups[team]:
+#         new_matchups = copy.deepcopy(matchups)
+#         new_schedule = copy.deepcopy(schedule)
+
+#         for i in range((n-1)*2):
+#             if new_schedule[team][i] != None or new_schedule[m[0]][i] != None:
+#                 continue
+
+#             new_schedule[team][i] = m
+#             new_matchups[team].remove(m)
+
+#             opposing = (team, "H" if m[1] == "A" else "A")
+#             new_schedule[m[0]][i] = opposing
+#             new_matchups[m[0]].remove(opposing)
+
+#             generate_schedules(n, new_matchups, new_schedule, team)
+#             break
+
+# # Function to generate all TTPs of size n
+# def generate_TTP(n):
+#     # Initialaize matchups array
+#     matchups = [[] for _ in range(n)]
+
+#     # Generate all possible matchups
+#     for i in range(n):
+#         for j in range(n):
+#             if i != j:
+#                 # Numpy append matchup away and home to row i
+#                 matchups[i].append((j, "H"))
+#                 matchups[i].append((j, "A"))
+    
+#     # print("TTP for %d teams:" % n)
+#     # for i in range(n):
+#     #     print("Team %d: %s" % (i, matchups[i]))
+                
+#     # Generate all possible TTP schedules
+#     generate_schedules(n, matchups, np.array([np.empty((n-1)*2, dtype=object) for _ in range(n)]))
+
+def counter():
+    global COUNT
+    COUNT += 1
+    print(COUNT)
+
+def print_schedules(n, schedules):
+    print("TTP for %d teams:" % n)
+    for schedule in schedules:
+        print("", schedule)
+
+def generate_matchups(n):
+    # Initialize matchups array
+    matchups = []
+
+    # Generate all possible matchups
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                matchups.append((i, j))
+    return matchups
+
+def generate_schedules(n, matchups, schedules, schedule=[]):
+    if len(schedule) is (n-1) * n:
+        schedules.append(schedule)
+        return
+
+    prev = []
+    index = len(schedule) % (n // 2)
+    
+    if index is not 0:
+        temp = schedule[-index:]
+        for t in temp:
+            prev.append(t[0])
+            prev.append(t[1])
+
+    for m in matchups:
+        if len(prev) > 0 and (m[0] in prev or m[1] in prev):
+            continue
+
+        new_matchups = matchups.copy()
+        new_matchups.remove(m)
+        new_schedule = schedule.copy()
+        new_schedule.append(m)
+
+        generate_schedules(n, new_matchups, schedules, new_schedule)
+        
+
+def generate_TTP(n):
+    schedules = []
+    matchups = generate_matchups(n)
+    print("Matchups:\n", matchups)
+    generate_schedules(n, matchups, schedules)
+
+    #print(len(schedules))
+    print_schedules(n, schedules[:20])
+
+if __name__ == "__main__":
+    # Set n_start and n_end equal to first and second command line arguments
+    try:
+        n_start = int(sys.argv[1])
+    except:
+        print("Usage:\n python TTP.py n_start n_end\n python TTP.py n")
+    
+    # If n_end is not provided, set it equal to n_start
+    try:
+        n_end = int(sys.argv[2])
+    except:
+        n_end = n_start
+
+    # Check if n_start or n_end is odd
+    if n_start % 2 != 0 or n_end % 2 != 0:
+        print("n_start and n_end must be even")
+        sys.exit(1)
+    elif n_start > n_end:
+        print("n_start must be less than or equal to n_end")
+        sys.exit(1)
+
+    # Run generate_TTP for each n in the range
+    for n in range(n_start, n_end + 1, 2):
+        generate_TTP(n)
