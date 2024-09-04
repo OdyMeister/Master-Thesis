b20ab45c3889de56fc1506111e0ed394d51ada32
OdyMeister <onixbl@gmail.com>
2024-09-04 18:16:32 +0200

    Distances between schedules can be found and distribution is plotted. Also when disregarding home/away games

--- calc.py
+++ calc.py
@@ -65,23 +65,15 @@ def verify_schedules(n, path):
             count += 1
 
 
-def get_current_round(schedule, n, i):
-    return
-
-
 def calc_distance(filepath, n):
     with open(filepath, "r") as file:
         schedules = []
-        count = 0
-        distances = {}
-        distances_separate = []
+        distances = []
 
         for line in file:
             raw_schedule = ast.literal_eval("[" + line + "]")
             schedule_chunks = [raw_schedule[i:i+n//2] for i in range(0, len(raw_schedule), n//2)]
             schedules.append(schedule_chunks)
-            distances[count] = []
-            count += 1
         
         for s in range(len(schedules)):
             for c in range(s+1, len(schedules)):
@@ -90,11 +82,36 @@ def calc_distance(filepath, n):
                     for m in schedules[s][r]:
                         if m not in schedules[c][r]:
                             distance += 1
-                distances[s].append(distance)
-                distances[c].append(distance)
-                distances_separate.append(distance)
+                distances.append(distance)
         
-        return distances, distances_separate
+        return distances
+    
+
+def calc_distance_reduced(filepath, n):
+    with open(filepath, "r") as file:
+        schedules = []
+        distances = []
+
+        count = 1
+
+        for line in file:
+            raw_schedule = ast.literal_eval("[" + line + "]")
+            schedule_chunks = [raw_schedule[i:i+n//2] for i in range(0, len(raw_schedule), n//2)]
+            schedules.append(schedule_chunks)
+            if count % 100000 == 0:
+                print(count)
+            count += 1
+        
+        for s in range(len(schedules)):
+            for c in range(s+1, len(schedules)):
+                distance = 0
+                for r in range(len(schedules[s])):
+                    for m in schedules[s][r]:
+                        if not (m in schedules[c][r] or (m[1], m[0]) in schedules[c][r]):
+                            distance += 1
+                distances.append(distance)
+        
+        return distances
 
 
 if __name__ == "__main__":
--- plot.py
+++ plot.py
@@ -43,37 +43,50 @@ def plot_upper_bound(max):
     plt.show()
 
 
-def plot_distances(file_path,n):
-    distances, separate = calc_distance(file_path, n)
+def plot_distances(file_path, n):
+    distances = calc_distance(file_path, n)
     name = file_path.split("\\")[-1].split(".")[0]
 
-    # x = [np.mean(distances[s]) for s in distances]
+    max_dist = calc_matchups(n)
+    min_dist = 2
 
-    # plt.hist(x)
-    # plt.xlabel("Average distances")
-    # plt.ylabel("Frequency")
-    # plt.title("Distribution of average distances per schedule")
-    # plt.savefig(f"Plots/Mean dist per schedule {name}.png")
-    # plt.show()
-
-    max_dist = max([max(distances[s]) for s in distances]) + 2
-    min_dist = min([min(distances[s]) for s in distances])
     x_axis = range(min_dist, max_dist)
 
-    plt.hist(separate, bins=x_axis, align='left', color='orange', alpha=0.9, edgecolor='black', linewidth=1)
+    plt.hist(distances, bins=x_axis, align='left', color='orange', alpha=0.9, edgecolor='black', linewidth=1)
     plt.xticks(x_axis)
     plt.xlabel("Distances")
     plt.ylabel("Frequency")
     plt.grid(alpha=0.5)
-    plt.title("Distribution of distances")
-    plt.savefig(f"Plots/General distribution of distances {name}.png")
+    plt.title("Distribution of distances between all schedules")
+    plt.savefig(f"Plots/Distribution of distances {name}.png")
+    plt.show()
+
+
+def plot_distances_reduced(file_path, n):
+    distances = calc_distance_reduced(file_path, n)
+    name = file_path.split("\\")[-1].split(".")[0]
+
+    max_dist = calc_matchups(n) + 1
+    min_dist = 0
+
+    x_axis = range(min_dist, max_dist)
+
+    plt.hist(distances, bins=x_axis, align='left', color='orange', alpha=0.9, edgecolor='black', linewidth=1)
+    plt.xticks(x_axis)
+    plt.xlabel("Distances")
+    plt.ylabel("Frequency")
+    plt.grid(alpha=0.5)
+    plt.title("Distribution of distances between all schedules (disregarding home/away)")
+    plt.savefig(f"Plots/Distribution of distances for reduced schedueles {name}.png")
     plt.show()
 
 
 if __name__ == "__main__":
     file_path = sys.argv[1]
     n = int(sys.argv[2])
-    plot_distances(file_path, n)
+
+    # plot_distances(file_path, n)
+    plot_distances_reduced(file_path, n)
 
     # plot_matchups(50)
     # plot_rounds(50)
