4cb541f8f7fd827870b944953ab4f622cd6d98c1
OdyMeister <onixbl@gmail.com>
2024-11-05 23:14:33 +0100

    Sampler home/away is broken, added comments though

--- sampler.py
+++ sampler.py
@@ -3,43 +3,56 @@ import sys
 from copy import copy
 from TTP import generate_matchups
 
-
+# Indicates whether a feasible schedule has been found
+# If so, the program will stop generating schedules
 FOUND = False
 
-
+# Prints a schedule round by round
 def print_schedule(schedule):
     print("Schedule found:")
     for i in range(len(schedule)):
         print(schedule[i])
 
-
+# Handles feasible schedules by:
+#  sorting the matchups in each round (to prevent duplicates being counted as different schedules)
+#  and append the schedule to a file
 def handle_schedule(n, schedule):
     formated = ""
 
+    # Sort the matchups in each round and append them to the formated string which will be written to the file
+    # The formatted simply contains the matchups in each round, separated by a space (so without brackets to save space)
     for i in range(len(schedule)):
         temp = sorted(schedule[i], key=lambda x: x[0])
         for m in temp:
             formated += f" {m[0]},{m[1]}"
     
+    # Append the formated string to the file
     with open(f"Schedules/Sampler/{n}.csv", "a") as file:
         file.write(formated[1:] + "\n")
 
-
+# Checks if a team is already playing in the current round
 def same_round(schedule, matchup, index):
     round = schedule[index[1]]
 
+    # Loops through the matchups in the current round
+    # Checks if either team in the new matchup is already playing, i.e. already in a matchup in the current round
     for m in round:
         if m[0] in matchup or m[1] in matchup:
             return True
         
     return False
 
-
+# Checks if a team is playing in the round before or after the current round
+# Does so by checking if the matchup with reversed home/away assignment is in the previous or next round
+# Order is reversed since each matchup only occurs once in a schedule
+# and thus only the reverse could be int the round before or after
 def back_to_back(schedule, matchup, index):
+    # If the current round is not the first round, check the previous round
     if index[1] != 0:
         prev_round = schedule[index[1]-1]
         if (matchup[1], matchup[0]) in prev_round:
             return True
+    # If the current round is not the last round, check the next round
     if index[1] < len(schedule)-1:
         next_round = schedule[index[1]+1]
         if (matchup[1], matchup[0]) in next_round:
@@ -47,15 +60,23 @@ def back_to_back(schedule, matchup, index):
 
     return False
 
-
+# Checks if a team is playing at home or away three times in a row
 def home_away_streak(schedule, matchup, index):
+    # Get the indexes of at most 3 rounds before and after the current round
     lower = max(0, index[1]-3)
     upper = min(len(schedule)-1, index[1]+3)
+    # Get the rounds in the range of the indexes
     rounds = schedule[lower:upper+1]
+    # Initialize the streaks for each team at 0
     streaks = {"home1": 0, "home2": 0, "away1": 0, "away2": 0}
 
     for round in rounds:
-        for m in round:
+        for i in range(len(round)):
+            m = round[i]
+
+            if i == index[0]:
+                continue #TODO
+
             if m[0] == matchup[0]:
                 streaks["home1"] += 1
                 streaks["away1"] = 0
@@ -69,9 +90,9 @@ def home_away_streak(schedule, matchup, index):
                 streaks["away2"] += 1
                 streaks["home2"] = 0
         
-        for key in streaks:
-            if streaks[key] == 3:
-                return True
+            for key in streaks:
+                if streaks[key] > 3:
+                    return True
 
     return False
 
