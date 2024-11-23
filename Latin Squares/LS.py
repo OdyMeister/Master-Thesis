82b0aa75a14394bbd3e6c2ad39e0ca8688192c0a
OdyMeister <onixbl@gmail.com>
2024-11-23 23:42:24 +0100

    double random backtraacking werkt!

--- Latin Squares/LS.py
+++ Latin Squares/LS.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import timeit
+import random
 
 COUNT = 0
 
@@ -15,7 +16,8 @@ def ls_random_backtracking(n, save=False):
     row, column = 1, 0
 
     # Keeps track of the numbers which are still available in the current row
-    current = [i for i in range(n)]
+    current = [num for num in range(n)]
+    random.shuffle(current)
 
     # Half-normalize the Latin Square
     for i in current:
@@ -26,13 +28,34 @@ def ls_random_backtracking(n, save=False):
 
     return LSs
 
+
+# This function generates all the Latin Squares of order n
+# It does so using double random backtracking
+def ls_double_random_backtracking(n, save=False):
+    # Make a list to store all the Latin Squares and initialize the first one
+    LSs = []
+    LS = np.zeros((n, n), dtype=int) - 1
+
+    # Keeps track of all the empty spots in the LS
+    indexes = [(i, j) for i in range(1, n) for j in range(n)]
+    random.shuffle(indexes)
+
+    # Half-normalize the Latin Square
+    for i in range(n):
+        LS[0, i] = i
+
+    # Recursively generates all Latin Squares of order n
+    ls_drb(n, indexes, LS, LSs, save=save)
+
+    return LSs
+
+
 # Recursive function to generate all Latin Squares of order n
 def ls_rb(n, i_row, i_column, current, LS, LSs, save=False):
     # If we have reached the end of the column, we have a Latin Square
     if i_row == n:
         global COUNT
         COUNT += 1
-        return
         if save:
             LSs.append(LS.copy())
         return
@@ -50,11 +73,11 @@ def ls_rb(n, i_row, i_column, current, LS, LSs, save=False):
         new_LS = LS.copy()
         new_LS[i_row, i_column] = num
 
-        # Update the row and column indices
+        # Update the row and column indexes
         new_i_column = i_column + 1
         new_i_row = i_row
 
-        # If the end of the row is reached, reset indices and current list
+        # If the end of the row is reached, reset indexes and current list
         if new_i_column == n:
             new_i_row += 1
             new_i_column = 0
@@ -63,31 +86,84 @@ def ls_rb(n, i_row, i_column, current, LS, LSs, save=False):
         ls_rb(n, new_i_row, new_i_column, new_current, new_LS, LSs)
 
 
-if __name__ == "__main__":
-    n_min = int(sys.argv[1])
-    n_max = int(sys.argv[2])
-    file = open("Count/LS_count.txt", "w")
+# Recursive function to generate all Latin Squares of order n using double random backtracking
+def ls_drb(n, indexes, LS, LSs, save=False):
+    if len(indexes) == 0:
+        global COUNT
+        COUNT += 1
+        if save:
+            LSs.append(LS.copy())
+        return
+    
+    # Get the current row and column indexes
+    i_row, i_column = indexes[0]
+
+    # Check if we can place any of the numbers up to n
+    for num in range(n):
+        # Get the current column and row
+        column = LS[:, i_column]
+        row = LS[i_row]
+
+        # Check if the number is already in the column
+        if num in column or num in row:
+            continue
+
+        # Copy everything
+        new_LS = LS.copy()
+        new_LS[i_row, i_column] = num
+        new_indexes = indexes[1:]
+
+        ls_drb(n, new_indexes, new_LS, LSs)
+
+
+def timer(n, generator):
+    # Running and timing the generation of all Latin Squares of order n
+    start_time = timeit.default_timer()
+    generator(n)
+    stop_time = timeit.default_timer()
+    runtime = stop_time - start_time
+
+    # Format the time
+    if runtime < 1:
+        time = f"{runtime * 1000:.2f} ms"
+    elif runtime < 60:
+        time = f"{runtime:.2f} s"
+    elif runtime < 3600:
+        time = f"{runtime / 60:.2f} min"
+    elif runtime < 3600 * 24:
+        time = f"{runtime / 3600:.2f} h"
+    else:
+        time = f"{runtime / (3600 * 24):.2f} days"
+
+    return time
+
+# Main function which calls the timers and collects the resulst
+def main(n_min, n_max):
+    global COUNT
+    file_rb = open("Count/LS_rb_count.txt", "w")
+    file_drb = open("Count/LS_drb_count.txt", "w")
 
     # Calculate number of Latin Squares of order up to n_max
-    for n in range(n_min, n_max, 2):
-        # Running and timing the generation of all Latin Squares of order n
-        start_time = timeit.default_timer()
-        ls_random_backtracking(n)
-        stop_time = timeit.default_timer()
-        elapsed_time = stop_time - start_time
-
-        # Format the time
-        if elapsed_time < 1:
-            time = f"{elapsed_time * 1000:.2f} ms"
-        elif elapsed_time < 60:
-            time = f"{elapsed_time:.2f} s"
-        elif elapsed_time < 3600:
-            time = f"{elapsed_time / 60:.2f} min"
-        else:
-            time = f"{elapsed_time / 3600:.2f} h"
-
-        file.write(f"n={n}: {COUNT}, {time}\n")
-
-        print(f"n={n}:", COUNT, time)
+    for n in range(n_min, n_max + 1, 2):
+        time = timer(n, ls_random_backtracking)
+        file_rb.write(f"n={n}: {COUNT}, {time}\n")
+        print(f"rb  n={n}:", COUNT, time)
         COUNT = 0
+
+        time = timer(n, ls_double_random_backtracking)
+        file_drb.write(f"n={n}: {COUNT}, {time}\n")
+        print(f"drb n={n}:", COUNT, time)
+        COUNT = 0
+
+        print()
+
+
+# Get arguments and run main function
+if __name__ == "__main__":
+    n_min = int(sys.argv[1])
+    try:
+        n_max = int(sys.argv[2])
+    except:
+        n_max = n_min
+    main(n_min, n_max)
         
