79f6150c79a352d574cc368b24d9e97b370409da
OdyMeister <onixbl@gmail.com>
2024-11-23 22:30:27 +0100

    Generating LSs using random backtracking works!

--- Latin Squares/LS.py
+++ Latin Squares/LS.py
@@ -1,46 +1,93 @@
 import numpy as np
-from plot import plot_violations
+import sys
+import timeit
 
-# Generate a random Latin Square of size n
-# Row-wise generation, thus columns are most likely invalid
-def random_LS(n):
-    LS = np.array([])
+COUNT = 0
 
-    for _ in range(n):
-        row = np.arange(n)
-        np.random.shuffle(row)
-        LS = np.append(LS, row)
+# This function generates all the Latin Squares of order n
+# It does so using random backtracking
+def ls_random_backtracking(n, save=False):
+    # Make a list to store all the Latin Squares and initialize the first one
+    LSs = []
+    LS = np.zeros((n, n), dtype=int) - 1
+
+    # Initialize the index for first row and column
+    row, column = 1, 0
+
+    # Keeps track of the numbers which are still available in the current row
+    current = [i for i in range(n)]
+
+    # Half-normalize the Latin Square
+    for i in current:
+        LS[0, i] = i
     
-    return LS.reshape(n, n)
+    # Recursively generates all Latin Squares of order n
+    ls_rb(n, row, column, current, LS, LSs, save=save)
 
-# Check the number of violations in a Latin Square
-def check_LS(n, LS):
-    violations = 0
+    return LSs
 
-    # Turning each column into a set and subtracting the length
-    # from n tells us how many numbers are missing
-    for i in range(n):
-        violations += n - len(set(LS[:, i]))
+# Recursive function to generate all Latin Squares of order n
+def ls_rb(n, i_row, i_column, current, LS, LSs, save=False):
+    # If we have reached the end of the column, we have a Latin Square
+    if i_row == n:
+        global COUNT
+        COUNT += 1
+        return
+        if save:
+            LSs.append(LS.copy())
+        return
     
-    return violations
+    for num in current:
+        # Get the current column
+        column = LS[:, i_column]
 
-# Main funcion to generate and check Latin Squares for violations
-def calc_violations(n_lower, n_upper, power):
-    results = {}
+        # Check if the number is already in the column
+        if num in column:
+            continue
 
-    # Outer loop for the size of the Latin Square
-    for n in range(n_lower, n_upper + 1, 2):
-        results[n] = []
+        # Removes the number from the current list and updates the Latin Square
+        new_current = [new_num for new_num in current if new_num != num]
+        new_LS = LS.copy()
+        new_LS[i_row, i_column] = num
 
-        # Inner loop saving the number of violations for each Latin Square
-        for _ in range(10**power):
-            ls = random_LS(n)
-            violations = check_LS(n, ls)
-            results[n].append(violations)
-    
-    return results
-        
+        # Update the row and column indices
+        new_i_column = i_column + 1
+        new_i_row = i_row
+
+        # If the end of the row is reached, reset indices and current list
+        if new_i_column == n:
+            new_i_row += 1
+            new_i_column = 0
+            new_current = np.array([i for i in range(n)])
+
+        ls_rb(n, new_i_row, new_i_column, new_current, new_LS, LSs)
+
+
 if __name__ == "__main__":
-    sample_size_power = 5
-    result = calc_violations(4, 50, sample_size_power)
-    plot_violations(result, sample_size_power, hist=True, line=False, show=False)
+    n_min = int(sys.argv[1])
+    n_max = int(sys.argv[2])
+    file = open("Count/LS_count.txt", "w")
+
+    # Calculate number of Latin Squares of order up to n_max
+    for n in range(n_min, n_max, 2):
+        # Running and timing the generation of all Latin Squares of order n
+        start_time = timeit.default_timer()
+        ls_random_backtracking(n)
+        stop_time = timeit.default_timer()
+        elapsed_time = stop_time - start_time
+
+        # Format the time
+        if elapsed_time < 1:
+            time = f"{elapsed_time * 1000:.2f} ms"
+        elif elapsed_time < 60:
+            time = f"{elapsed_time:.2f} s"
+        elif elapsed_time < 3600:
+            time = f"{elapsed_time / 60:.2f} min"
+        else:
+            time = f"{elapsed_time / 3600:.2f} h"
+
+        file.write(f"n={n}: {COUNT}, {time}\n")
+
+        print(f"n={n}:", COUNT, time)
+        COUNT = 0
+        
---
+++ Latin Squares/random_LS.py
@@ -0,0 +1,46 @@
+import numpy as np
+from plot import plot_violations
+
+# Generate a random Latin Square of size n
+# Row-wise generation, thus columns are most likely invalid
+def random_LS(n):
+    LS = np.array([])
+
+    for _ in range(n):
+        row = np.arange(n)
+        np.random.shuffle(row)
+        LS = np.append(LS, row)
+    
+    return LS.reshape(n, n)
+
+# Check the number of violations in a Latin Square
+def check_LS(n, LS):
+    violations = 0
+
+    # Turning each column into a set and subtracting the length
+    # from n tells us how many numbers are missing
+    for i in range(n):
+        violations += n - len(set(LS[:, i]))
+    
+    return violations
+
+# Main funcion to generate and check Latin Squares for violations
+def calc_violations(n_lower, n_upper, power):
+    results = {}
+
+    # Outer loop for the size of the Latin Square
+    for n in range(n_lower, n_upper + 1, 2):
+        results[n] = []
+
+        # Inner loop saving the number of violations for each Latin Square
+        for _ in range(10**power):
+            ls = random_LS(n)
+            violations = check_LS(n, ls)
+            results[n].append(violations)
+    
+    return results
+        
+if __name__ == "__main__":
+    sample_size_power = 5
+    result = calc_violations(4, 50, sample_size_power)
+    plot_violations(result, sample_size_power, hist=True, line=False, show=False)
